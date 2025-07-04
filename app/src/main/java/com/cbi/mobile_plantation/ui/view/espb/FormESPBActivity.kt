4b072305398cfc84df9c3a1c6d218c4ec4287d6e
Dendy Surya <dendysurya15@gmail.com>
2025-07-04 08:28:09 +0700

    build ke 3.10 SPE - transporter name

--- app/src/main/java/com/cbi/mobile_plantation/ui/view/espb/FormESPBActivity.kt
+++ app/src/main/java/com/cbi/mobile_plantation/ui/view/espb/FormESPBActivity.kt
@@ -147,6 +147,7 @@ class FormESPBActivity : AppCompatActivity() {
     private var kemandoran_id = "NULL"
     private var pemuat_nik = "NULL"
     private var tph1NoIdPanen = ""
+    private var selectedTransporterName = ""
     private lateinit var warningText: TextView
     private lateinit var warningCard: CardView
 
@@ -1123,6 +1124,7 @@ class FormESPBActivity : AppCompatActivity() {
                 val infoTotalJjg = screenshotLayout.findViewById<View>(R.id.infoTotalJjg)
                 val infoTotalTransaksi = screenshotLayout.findViewById<View>(R.id.infoTotalTransaksi)
                 val infoNoESPB = screenshotLayout.findViewById<View>(R.id.infoNoESPB)
+                val infoTransporter = screenshotLayout.findViewById<View>(R.id.infoTransporter)
                 val infoDriver = screenshotLayout.findViewById<View>(R.id.infoDriver)
                 val infoNopol = screenshotLayout.findViewById<View>(R.id.infoNopol)
                 val infoPemuat = screenshotLayout.findViewById<View>(R.id.infoPemuat)
@@ -1212,6 +1214,10 @@ class FormESPBActivity : AppCompatActivity() {
                 setInfoData(infoTotalJjg, "Total Janjang", ": $totalJjg")
                 setInfoData(infoTotalTransaksi, "Jumlah Transaksi", ": $tphCount")
                 setInfoData(infoNoESPB, "E-SPB", ": $noESPBStr")
+                if (selectedTransporterId == 0) {
+                    selectedTransporterName = "Internal"
+                }
+                setInfoData(infoTransporter, "Transporter", ": $selectedTransporterName")
                 setInfoData(infoDriver, "Driver", ": $driver")
                 setInfoData(infoNopol, "Nomor Polisi", ": $selectedNopol")
                 setInfoData(infoPemuat, "Pemuat", ": $pemuatNama")
@@ -1735,6 +1741,12 @@ class FormESPBActivity : AppCompatActivity() {
                 } catch (e: Exception) {
                     AppLogger.e("Error finding selectedTransporterId: ${e.message}")
                     0
+                }
+                selectedTransporterName = try {
+                    transporterList.find { it.nama == selectedItem }?.nama!!
+                } catch (e: Exception) {
+                    AppLogger.e("Error finding selectedTransporterName: ${e.message}")
+                    ""
                 }
                 Log.d(
                     "FormESPBActivityTransporter",
